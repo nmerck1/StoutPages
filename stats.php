a3c409b1ee7d3c4cfe948ae1c319540d52a161ad
nmerck1 <nathanielmerck@yahoo.com>
2017-04-14 00:12:04 -0400

    pie chart added chart to stats page

--- stats.php
+++ stats.php
@@ -16,6 +16,41 @@ echo <<< HTML
 	<link href="https://fonts.googleapis.com/css?family=Poiret+One|Tangerine" rel="stylesheet">
     <!--        JavaScript      -->
     
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Beer', 'Votes'],
+          ['Guinness',          2],
+          ['Blue Moon',         2],
+          ['Samuel Adams',      2],
+          ['Heineken',          2],
+          ['Corona',            2],
+          ['Stella Artois',     2],
+          ['Budweiser',         2],
+          ['Sierra Nevada',     2],
+          ['Yuengling',         2],
+          ['Hoegaarden',        2],
+          ['Pilsner Urquell',   2],
+          ['Pabst Blue Ribbon', 2],
+          ['Coors Light',       2],
+          ['Stone Pale Ale',    2],
+          ['Rolling Rock',      2]
+        ]);
+
+        var options = {
+          title: 'Pie chart for Votes',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  
 </head>
 
 <body>
@@ -66,7 +101,7 @@ echo <<< HTML
             
             
             
-            
+        <div id="donutchart" style="width: 900px; height: 500px;"></div>
             
             
             
